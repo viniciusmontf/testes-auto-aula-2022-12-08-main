7e437f945027be4bd4ef39de7baf2541385d1788
Vinicius Monteiro <viniciusmonteiro30@gmail.com>
2022-12-14 22:09:56 -0300

    adicionando números e letras, teste passando

--- src/romanos.ts
+++ src/romanos.ts
@@ -1,24 +1,8 @@
 
 class Romanos {
-    private arabicos = [
-        1,
-        5,
-        10,
-        50,
-        100,
-        500,
-        1000
-    ]
+    private arabicos = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     
-    private romanos = [
-        'I',
-        'V',
-        'X',
-        'L',
-        'C',
-        'D',
-        'M'
-    ]
+    private romanos = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
 
     converte(arabico: number):string{
         
